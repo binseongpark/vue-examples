9dfb5303f189fce7ccc1e5b5d224e27ae293791f
박성빈 <kled@kakao.com>
2022-01-18 18:34:00 +0900

    fix .

---
+++ vue2-board/src/api/mock.js
@@ -0,0 +1,88 @@
+export default {
+  list: [
+    {
+      id: 1,
+      name: 'API AutoFlow'
+    },
+    {
+      id: 2,
+      name: 'Auth0'
+    },
+    {
+      id: 3,
+      name: 'AxonIQ'
+    },
+    {
+      id: 4,
+      name: 'Axual'
+    },
+    {
+      id: 5,
+      name: 'Conviva'
+    },
+    {
+      id: 6,
+      name: 'Dbvisit'
+    },
+    {
+      id: 7,
+      name: 'Gitlab'
+    },
+    {
+      id: 8,
+      name: 'Gremlin'
+    },
+    {
+      id: 9,
+      name: 'Imply'
+    },
+    {
+      id: 10,
+      name: 'Instana'
+    },
+    {
+      id: 11,
+      name: 'KrakenD'
+    },
+    {
+      id: 12,
+      name: 'Kubecost'
+    },
+    {
+      id: 13,
+      name: 'Lumen'
+    },
+    {
+      id: 14,
+      name: 'OceanBlue Cloud'
+    },
+    {
+      id: 15,
+      name: 'Okta'
+    },
+    {
+      id: 16,
+      name: 'Rancher'
+    },
+    {
+      id: 17,
+      name: 'Sonatype'
+    },
+    {
+      id: 18,
+      name: 'Speedscale'
+    },
+    {
+      id: 19,
+      name: 'SUSE'
+    },
+    {
+      id: 20,
+      name: 'Tigera'
+    },
+    {
+      id: 21,
+      name: 'WSO2'
+    },
+  ]
+}
